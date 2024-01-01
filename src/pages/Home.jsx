18a026e50755030afb26950a1c037608eb0535b5
kunal <kunaljoshi0603@gmail.com>
2024-01-01 18:05:11 +0530

    Removed S.no

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,6 @@ const Home = () => {
           <table className="min-w-full bg-white shadow-md rounded-xl">
             <thead>
               <tr className="bg-blue-gray-100 text-gray-700">
-                <th className="py-3 px-4 text-left">S.No</th>
                 <th className="py-3 px-4 text-left">Name</th>
                 <th className="py-3 px-4 text-left">Email</th>
                 <th className="py-3 px-4 text-left">Role</th>
@@ -74,7 +73,6 @@ const Home = () => {
             <tbody className="text-blue-gray-900">
               {users.map((user, index) => (
                 <tr key={index} className="border-b border-blue-gray-200">
-                  <td className="py-3 px-4">{index + 1}</td>
                   <td className="py-3 px-4">{user?.name}</td>
                   <td className="py-3 px-4">{user?.email}</td>
                   <td className="py-3 px-4">{user?.role}</td>
